18c9b897848fc51d9843a5308a74743a36ba0616
wanglin <wang_lin2010@sina.com>
2018-10-24 22:21:22 +0800

    person

---
+++ JavaWeb/src/com/atguigu/Person.java
@@ -0,0 +1,31 @@
+package com.atguigu;
+
+public class Person {
+    private  String pNAME;
+    private  int pAGE;
+    private  String pSEX;
+
+    public String getNAME() {
+        return pNAME;
+    }
+
+    public void setNAME(String pNAME) {
+        this.pNAME = pNAME;
+    }
+
+    public int getAGE() {
+        return pAGE;
+    }
+
+    public void setAGE(int pAGE) {
+        this.pAGE = pAGE;
+    }
+
+    public String getSEX() {
+        return pSEX;
+    }
+
+    public void setSEX(String pSEX) {
+        this.pSEX = pSEX;
+    }
+}
